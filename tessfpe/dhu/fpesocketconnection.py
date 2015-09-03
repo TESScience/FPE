6b2154b7a62a850c2e96c4771757f477bf624673
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-09-02 21:01:24 -0400

    Orchestrating uploading files to the Observatory Simulator

--- tessfpe/dhu/fpesocketconnection.py
+++ tessfpe/dhu/fpesocketconnection.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 
-
 def is_prompt(text):
-    """Determines if the text contains an FPE prompt"""
-    return "FPE1> " in text or \
-           "FPE2> " in text or \
-           "FPE3> " in text or \
-           "FPE4> " in text
+    """Checks to see if text is an FPE prompt, returns a match object if so"""
+    import re
+    return re.match(r'FPE[1-9]> ', text)
 
 
 class FPESocketConnection(object):
     """An object for encapsulating an open socket connection with the Observatory Simulator"""
 
-    def __init__(self, port):
+    def __init__(self, port, debug=False):
         import socket
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
         self.socket.connect(('192.168.100.1', int(self.port)))
+        self._debug = debug
         self.get_prompt()
-        self.ready = True
 
     def close(self):
         """Close the FPE Socket Connection"""
@@ -36,20 +33,26 @@ class FPESocketConnection(object):
         """Send a string to the FPE DHU controller"""
         self.socket.sendall((b'\n' + command + b'\n').encode())
 
-    def send_command(self, command, chars=1024):
+    def send_command(self, command, chars=1024, prompt_skips=10):
         """Send a command to the FPE"""
-        if not self.ready:
-            self.wait_for_prompt()
-            self.ready = True
-        self.send(command)
-        data = self.socket.recv(chars)
-        return repr(data)
+        self.socket.sendall((command + b'\n').encode())
+        data = ''
+        for _ in range(prompt_skips):
+            # Skip the prompt until we get real data
+            data = self.socket.recv(chars)
+            if self._debug:
+                print data,
+            if not is_prompt(data):
+                break
+        return data.rstrip('\n\r')
 
-    def wait_for_prompt(self):
+    def wait_for_prompt(self, chars=1024):
         """Dumps output waiting for the FPE Prompt"""
         out = ""
         while not is_prompt(out):
-            out = repr(self.socket.recv(1024)).encode()
+            out = str(self.socket.recv(chars)).encode()
+            if self._debug:
+                print out,
 
     def get_prompt(self):
         """Gets the FPE prompt"""
